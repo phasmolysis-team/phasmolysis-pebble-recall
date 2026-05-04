022d4641c0e1330ad6b8c8a6f0ab09ac867a6cf6
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2026-05-04 10:59:18 +0800

    models: add users model
    Signed-off-by: Soc Virnyl Estela <contact@uncomfyhalomacro.pl>

---
+++ backend/app/models/users/__init__.py
@@ -0,0 +1,41 @@
+import datetime
+import sqlalchemy as sa
+from typing import Annotated, Literal, List
+
+from sqlmodel import SQLModel, Field, Column
+from pydantic import EmailStr, BaseModel
+
+
+class BaseUsers(SQLModel, table=False):
+    username: str | None
+    email: EmailStr
+    contact_number: str
+    password: str
+
+
+class Users(BaseUsers, table=True):
+    id: Annotated[int | None, Field(primary_key=True, default=None)]
+    kind: Annotated[List[Literal['patient', 'professional']], Field(sa_column=Column(sa.TEXT))]
+    professional_license_id: Annotated[str, Field(sa_column=Column(sa.TEXT, nullable=True))]
+    created_at: Annotated[
+        datetime.datetime,
+        Field(sa_column=Column(sa.TIMESTAMP(timezone=True), nullable=True)),
+    ]
+    updated_at: Annotated[
+        datetime.datetime,
+        Field(sa_column=Column(sa.TIMESTAMP(timezone=True), nullable=True)),
+    ]
+    disabled: Annotated[bool, Field(sa_column=Column(sa.BOOLEAN, nullable=False))]
+
+
+class UserWithoutPassword(BaseModel):
+    username: str | None
+    email: EmailStr
+    contact_number: str
+
+
+class UpdateUser(BaseModel):
+    username: str | None = None
+    email: EmailStr | None = None
+    contact_number: str | None = None
+    password: str | None = None
