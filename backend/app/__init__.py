54079f28fce7ee72c9ea4006dd37a09f34f501a8
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2026-05-04 11:05:39 +0800

    chore: prepare root route
    Signed-off-by: Soc Virnyl Estela <contact@uncomfyhalomacro.pl>

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,8 +1,17 @@
-from fastapi import FastAPI
 from app.core.config import settings
-app = FastAPI(root_path=settings.API_ROOT)
+from .api import auth, users
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
 
+app = FastAPI(root_path="/api")
 
-@app.get("/healthz")
-async def check_endpoint():
-    return {"message": "Hello, World"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=list(settings.ORIGINS),
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(auth.router, prefix="/auth", tags=["auth"])
+app.include_router(users.router, tags=["users"])
