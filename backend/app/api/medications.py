c09ee26b9cba70ef83d53b243a4c1a92e3d97bf3
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2026-05-05 16:56:06 +0800

    fix: forgot to add POST /medication/logs for adding new medication logs
    Signed-off-by: Soc Virnyl Estela <contact@uncomfyhalomacro.pl>

--- backend/app/api/medications.py
+++ backend/app/api/medications.py
@@ -1,14 +1,15 @@
 from app.models.medication import TMedication, FreqUnit, DosageUnit
 import datetime
 from app.models.medication_logs import MedicationLogs, MedicationLogsWithTimestamp
-from uuid import uuid7
-from pydantic import BaseModel, Json
+from uuid import uuid7, UUID
+from pydantic import BaseModel, Json, UUID7
 from app.models.moods import MoodLogs, MoodLogsWithTimestamp
 from app.middlewares.auth import check_if_logged_in, check_encrypted_cookie_auth
 from app.core.security.jwt_service import Claims
 from starlette.status import (
     HTTP_401_UNAUTHORIZED,
     HTTP_500_INTERNAL_SERVER_ERROR,
+    HTTP_404_NOT_FOUND,
 )
 from app.models.users import Users
 
@@ -20,13 +21,20 @@ from sqlmodel import select, desc
 
 router = APIRouter()
 
+
+class MedicationLogsParams(BaseModel):
+    medication: UUID7 | str
+    side_effects: str
+
+
 class TMedicationParams(BaseModel):
     name: str
     frequency: int
-    frequency_unit: FreqUnit |str
+    frequency_unit: FreqUnit | str
     frequency_times_per_unit: int
     recommended_dosage: float
     recommended_dosage_unit: DosageUnit | str
+
 
 @router.get(path="/medications/logs")
 async def get_medication_logs(
@@ -50,6 +58,7 @@ async def get_medication_logs(
         return [MedicationLogsWithTimestamp(**log.model_dump()) for log in logs]
     return logs
 
+
 @router.get(path="/medications/logs/latest")
 async def get_medication_logs_latest(
     request: Request,
@@ -66,7 +75,9 @@ async def get_medication_logs_latest(
         raise HTTPException(status_code=HTTP_500_INTERNAL_SERVER_ERROR)
 
     statement = (
-        select(MedicationLogs).where(MedicationLogs.user_id == user.id).order_by(desc(MedicationLogs.id))
+        select(MedicationLogs)
+        .where(MedicationLogs.user_id == user.id)
+        .order_by(desc(MedicationLogs.id))
     )
     results = await session.exec(statement)
     logs = results.first()
@@ -82,7 +93,7 @@ async def get_medication_logs_with_date(
     session: Annotated[AsyncSession, Depends(get_session)],
     claims: Annotated[Claims, Depends(check_encrypted_cookie_auth)],
     is_logged_in: Annotated[bool, Depends(check_if_logged_in)],
-    date: datetime.datetime
+    date: datetime.datetime,
 ):
     if not is_logged_in:
         raise HTTPException(status_code=HTTP_401_UNAUTHORIZED)
@@ -93,7 +104,9 @@ async def get_medication_logs_with_date(
         raise HTTPException(status_code=HTTP_500_INTERNAL_SERVER_ERROR)
 
     statement = (
-        select(MedicationLogs).where(MedicationLogs.user_id == user.id).order_by(desc(MedicationLogs.id))
+        select(MedicationLogs)
+        .where(MedicationLogs.user_id == user.id)
+        .order_by(desc(MedicationLogs.id))
     )
     day = date.day
     results = await session.exec(statement)
@@ -105,12 +118,14 @@ async def get_medication_logs_with_date(
         return ret
     return logs
 
+
 @router.post(path="/medications/logs")
 async def add_medication_logs(
     request: Request,
     session: Annotated[AsyncSession, Depends(get_session)],
     claims: Annotated[Claims, Depends(check_encrypted_cookie_auth)],
     is_logged_in: Annotated[bool, Depends(check_if_logged_in)],
+    payload: MedicationLogsParams,
 ):
     if not is_logged_in:
         raise HTTPException(status_code=HTTP_401_UNAUTHORIZED)
@@ -120,14 +135,48 @@ async def add_medication_logs(
     if user is None:
         raise HTTPException(status_code=HTTP_500_INTERNAL_SERVER_ERROR)
 
-    pass
+    if user.id is None:
+        raise HTTPException(status_code=HTTP_500_INTERNAL_SERVER_ERROR)
+
+    if isinstance(payload.medication, UUID):
+        statement = select(TMedication).where(TMedication.id == payload.medication)
+        results = await session.exec(statement)
+        tmedication = results.first()
+        if tmedication is None:
+            raise HTTPException(
+                status_code=HTTP_404_NOT_FOUND,
+                detail="no such medication found based on id",
+            )
+        log = MedicationLogs(
+            id=uuid7(),
+            user_id=user.id,
+            medication_id=tmedication.id,
+            medication_name=tmedication.name,
+            user_noted_side_effects=payload.side_effects,
+        )
+        session.add(log)
+        await session.commit()
+        await session.refresh(log)
+        return log
+    log = MedicationLogs(
+        id=uuid7(),
+        user_id=user.id,
+        medication_id=None,
+        medication_name=payload.medication,
+        user_noted_side_effects=payload.side_effects,
+    )
+    session.add(log)
+    await session.commit()
+    await session.refresh(log)
+    return log
+
 
 @router.post(path="/medications")
 async def add_new_medication(
     request: Request,
     session: Annotated[AsyncSession, Depends(get_session)],
     is_logged_in: Annotated[bool, Depends(check_if_logged_in)],
-    payload: TMedicationParams
+    payload: TMedicationParams,
 ):
     if not is_logged_in:
         raise HTTPException(status_code=HTTP_401_UNAUTHORIZED)
@@ -139,6 +188,7 @@ async def add_new_medication(
     await session.commit()
     await session.refresh(medicine)
     return medicine
+
 
 @router.get(path="/medications")
 async def get_medications(
@@ -152,7 +202,3 @@ async def get_medications(
     statement = select(TMedication)
     results = await session.exec(statement)
     return results.all()
-
-
-
-    
