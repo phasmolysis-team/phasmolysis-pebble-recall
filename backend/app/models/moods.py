7e7435065a4e8807365a6522130813bcfa74b247
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2026-05-05 10:19:36 +0800

    feat: create model for MoodLogs
    Signed-off-by: Soc Virnyl Estela <contact@uncomfyhalomacro.pl>

---
+++ backend/app/models/moods.py
@@ -0,0 +1,23 @@
+from uuid import uuid7
+from pydantic import UUID7
+from sqlmodel import SQLModel, Field
+import sqlalchemy as sa
+from typing import Annotated
+
+
+# NOTE: A mood is just a wrapper for emotions
+class MoodLogs(SQLModel, table=True):
+    id: Annotated[
+        UUID7,
+        Field(
+            sa_column=sa.Column(
+                sa.UUID(as_uuid=True),
+            ),
+        ),
+    ] = uuid7()
+    user_id: Annotated[
+        int,
+        Field(sa_column=sa.Column(sa.Integer)),
+    ]
+    valence: Annotated[float, Field(sa_column=sa.Column(sa.Float))] = 0.0
+    arousal: Annotated[float, Field(sa_column=sa.Column(sa.Float))] = 0.0
