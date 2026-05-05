797b41aec25a67e34b807117d7061606c97a3888
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2026-05-05 21:40:02 +0800

    fix: set annotation of ORIGINS to `str | set[str]`
    Signed-off-by: Soc Virnyl Estela <contact@uncomfyhalomacro.pl>

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -17,7 +17,7 @@ class AuthConfig(BaseModel):
 class Settings(BaseSettings):
     AUTH: Annotated[AuthConfig, Field()] = AuthConfig()
     PG_URL: Annotated[str, Field()] = ""
-    ORIGINS: Annotated[set[str], Field()] = set()
+    ORIGINS: Annotated[str | set[str], Field()] = set()
     API_ROOT: Annotated[str, Field()] = "/api"
     PORT: Annotated[int, Field()] = 8080
     HOST: Annotated[str, Field()] = "localhost"
