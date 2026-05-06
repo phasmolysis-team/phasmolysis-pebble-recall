5633459c9a0921c3d4faa75bda6b2e44db43c867
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2026-05-06 22:10:00 +0800

    fix: there is an issue with how assets are handled. for now i just copy it over to dist from src/assets
    Signed-off-by: Soc Virnyl Estela <contact@uncomfyhalomacro.pl>

--- frontend/src/rock_composite.tsx
+++ frontend/src/rock_composite.tsx
@@ -6,7 +6,7 @@ type RockCompositeProps = {
 	tint: string;
 };
 export function RockComposite({ hat, eyes, base, tint }: RockCompositeProps) {
-	const src_link = "./src/assets/rocks/";
+	const src_link = "./assets/rocks/";
 	const hat_src_img = src_link + "hat" + hat.toString() + ".png";
 	const eye_src_img = src_link + "eye" + eyes.toString() + ".png";
 	const base_src_img = src_link + "body" + base.toString() + ".png";
